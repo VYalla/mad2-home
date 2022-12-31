bd0e3f33854b5858acb9c4e4d632e493fd39156f
VYalla <91841638+VYalla@users.noreply.github.com>
2022-12-30 16:01:47 -0800

    first add

---
+++ android/app/src/main/kotlin/com/example/jaghub_homepage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jaghub_homepage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,387 @@
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import '../flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:mad2_db_dataobjects/API.dart';
+import 'package:mad2_db_dataobjects/event_data.dart';
+import 'package:mad2_db_dataobjects/group_data.dart';
+import 'package:mad2_db_dataobjects/prize_data.dart';
+import 'package:mad2_db_dataobjects/user_data.dart';
+
+class HomePageCopyWidget extends StatefulWidget {
+  const HomePageCopyWidget({Key? key}) : super(key: key);
+
+  @override
+  _HomePageCopyWidgetState createState() => _HomePageCopyWidgetState();
+}
+
+class _HomePageCopyWidgetState extends State<HomePageCopyWidget> {
+  final _unfocusNode = FocusNode();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  late UserData currentUser;
+  late String username;
+
+  late Widget username_W; 
+
+  _getUsername() {
+    API().getCurrentUserData().then((value) {
+      currentUser = value;
+      username = currentUser.name;
+
+      username_W = Text(username);
+    });
+  }
+
+  @override
+  void dispose() {
+    _unfocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Align(
+                  alignment: AlignmentDirectional(-0.75, -0.35),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 45, 0, 0),
+                    child: Text(
+                      'Welcome Back',
+                      style: FlutterFlowTheme.of(context).subtitle2.override(
+                            fontFamily: 'Poppins',
+                            color: FlutterFlowTheme.of(context).primaryText,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w200,
+                          ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0.45, 0.05),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 85, 0),
+                    child: username_W
+                    
+                    /*
+                    Text(
+                      "Vishal Yalla",
+                      style: FlutterFlowTheme.of(context).title1.override(
+                            fontFamily: 'Poppins',
+                            fontSize: 40,
+                          ),
+                    ),*/
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0, 0),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(25, 0, 0, 0),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 1.53,
+                            height: 177,
+                            constraints: BoxConstraints(
+                              maxWidth: 153,
+                              maxHeight: 177,
+                            ),
+                            decoration: BoxDecoration(
+                              color: FlutterFlowTheme.of(context).primaryColor,
+                              borderRadius: BorderRadius.circular(34),
+                            ),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 35, 5, 0),
+                                  child: FFButtonWidget(
+                                    onPressed: () {
+                                      print('Button pressed ...');
+                                    },
+                                    text: '20',
+                                    icon: Icon(
+                                      Icons.star_rate,
+                                      size: 30,
+                                    ),
+                                    options: FFButtonOptions(
+                                      elevation: 0,
+                                      width: 130,
+                                      color: FlutterFlowTheme.of(context)
+                                          .primaryColor,
+                                      textStyle: FlutterFlowTheme.of(context)
+                                          .title1
+                                          .override(
+                                            fontFamily: 'Poppins',
+                                            color: FlutterFlowTheme.of(context)
+                                                .secondaryBackground,
+                                            fontSize: 55,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                      borderSide: BorderSide(
+                                        color: Colors.transparent,
+                                        width: 1,
+                                      ),
+                                      borderRadius: 8,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  'Total Points',
+                                  style: FlutterFlowTheme.of(context)
+                                      .bodyText1
+                                      .override(
+                                        fontFamily: 'Poppins',
+                                        color: FlutterFlowTheme.of(context)
+                                            .primaryBtnText,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 1.53,
+                            height: 177,
+                            constraints: BoxConstraints(
+                              maxWidth: 153,
+                              maxHeight: 177,
+                            ),
+                            decoration: BoxDecoration(
+                              color: FlutterFlowTheme.of(context)
+                                  .primaryBackground,
+                              borderRadius: BorderRadius.circular(34),
+                            ),
+                            child: SingleChildScrollView(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsetsDirectional.fromSTEB(
+                                        0, 35, 5, 0),
+                                    child: FFButtonWidget(
+                                      onPressed: () {
+                                        print('Button pressed ...');
+                                      },
+                                      text: '05',
+                                      icon: Icon(
+                                        Icons.bar_chart,
+                                        size: 25,
+                                      ),
+                                      options: FFButtonOptions(
+                                        elevation: 0,
+                                        width: 130,
+                                        color: FlutterFlowTheme.of(context)
+                                            .primaryBackground,
+                                        textStyle: FlutterFlowTheme.of(context)
+                                            .title1
+                                            .override(
+                                              fontFamily: 'Poppins',
+                                              color:
+                                                  FlutterFlowTheme.of(context)
+                                                      .primaryText,
+                                              fontSize: 55,
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                        borderSide: BorderSide(
+                                          color: Colors.transparent,
+                                          width: 1,
+                                        ),
+                                        borderRadius: 8,
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    'School Rank',
+                                    style: FlutterFlowTheme.of(context)
+                                        .bodyText1
+                                        .override(
+                                          fontFamily: 'Poppins',
+                                          color: FlutterFlowTheme.of(context)
+                                              .secondaryText,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0, 0),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(25, 0, 0, 0),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 1.53,
+                            height: 177,
+                            constraints: BoxConstraints(
+                              maxWidth: 153,
+                              maxHeight: 177,
+                            ),
+                            decoration: BoxDecoration(
+                              color: FlutterFlowTheme.of(context)
+                                  .primaryBackground,
+                              borderRadius: BorderRadius.circular(34),
+                            ),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 35, 5, 0),
+                                  child: FFButtonWidget(
+                                    onPressed: () {
+                                      print('Button pressed ...');
+                                    },
+                                    text: '12',
+                                    icon: Icon(
+                                      Icons.place,
+                                      size: 30,
+                                    ),
+                                    options: FFButtonOptions(
+                                      elevation: 0,
+                                      width: 130,
+                                      color: FlutterFlowTheme.of(context)
+                                          .primaryBackground,
+                                      textStyle: FlutterFlowTheme.of(context)
+                                          .title1
+                                          .override(
+                                            fontFamily: 'Poppins',
+                                            color: FlutterFlowTheme.of(context)
+                                                .primaryText,
+                                            fontSize: 55,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                      borderSide: BorderSide(
+                                        color: Colors.transparent,
+                                        width: 1,
+                                      ),
+                                      borderRadius: 8,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  'Events Attended',
+                                  style: FlutterFlowTheme.of(context)
+                                      .bodyText1
+                                      .override(
+                                        fontFamily: 'Poppins',
+                                        color: FlutterFlowTheme.of(context)
+                                            .secondaryText,
+                                        fontWeight: FontWeight.w300,
+                                      ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 1.53,
+                            height: 177,
+                            constraints: BoxConstraints(
+                              maxWidth: 153,
+                              maxHeight: 177,
+                            ),
+                            decoration: BoxDecoration(
+                              color: FlutterFlowTheme.of(context)
+                                  .primaryBackground,
+                              borderRadius: BorderRadius.circular(34),
+                            ),
+                            child: SingleChildScrollView(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsetsDirectional.fromSTEB(
+                                        0, 35, 5, 0),
+                                    child: FFButtonWidget(
+                                      onPressed: () {
+                                        print('Button pressed ...');
+                                      },
+                                      text: '05',
+                                      icon: Icon(
+                                        Icons.emoji_events,
+                                        size: 25,
+                                      ),
+                                      options: FFButtonOptions(
+                                        elevation: 0,
+                                        width: 130,
+                                        color: FlutterFlowTheme.of(context)
+                                            .primaryBackground,
+                                        textStyle: FlutterFlowTheme.of(context)
+                                            .title1
+                                            .override(
+                                              fontFamily: 'Poppins',
+                                              color:
+                                                  FlutterFlowTheme.of(context)
+                                                      .primaryText,
+                                              fontSize: 55,
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                        borderSide: BorderSide(
+                                          color: Colors.transparent,
+                                          width: 1,
+                                        ),
+                                        borderRadius: 8,
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    'Item Purchased',
+                                    style: FlutterFlowTheme.of(context)
+                                        .bodyText1
+                                        .override(
+                                          fontFamily: 'Poppins',
+                                          color: FlutterFlowTheme.of(context)
+                                              .secondaryText,
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
